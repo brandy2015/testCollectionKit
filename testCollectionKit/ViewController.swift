f6cb94e8df607ed627e09dab31d6c9e588d062f4
XYZ <10484324+brandy2015@users.noreply.github.com>
2021-02-22 08:20:48 +0800

    Initial Commit

---
+++ testCollectionKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testCollectionKit
+//
+//  Created by zhangzihao on 2021/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
